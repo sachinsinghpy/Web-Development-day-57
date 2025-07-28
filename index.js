91ae5a747e02744ca8a5f3cae927a06a6de983d1
Sachin Kumar <skraja012345678@gmail.com>
2025-07-28 19:49:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("I am a tutorial on Loops")
+let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+// let obj = {
+//     name : "Sachin",
+//     role : "Programmer",
+//     company : "Learn AI"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element)
+// }
+// for (const c  of "Sachin") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<6000){
+//     console.log(i)
+//     i++;
+// } 
+let i = 0;
+
+do{
+     console.log(i)
+    i++; 
+}
+while (i<600);
